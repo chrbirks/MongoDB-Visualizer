ff052a4ce227f0210badad68b8c12c7246651357
Christian Birk Sørensen <chrbirks@gmail.com>
2016-05-14 19:27:50 +0200

    First version of temperature matrix plotting
    Will only plot one record for now. Beginning implementation of
    slider/selector.

--- mongodb-visualizer.py
+++ mongodb-visualizer.py
@@ -1,6 +1,16 @@
 import pymongo
-import plotly.plotly as pl
+import numpy
+#import colorlover as cl
+
+# Offline plotting
+import plotly
+#plotly.offline.init_notebook_mode()
+# Cloud plotting
+#import plotly.plotly as py
+#py.sign_in('chrbirks', 'wi7e2kfa8h')
+
 from plotly.tools import FigureFactory as FF
+import plotly.graph_objs as go
 
 client = pymongo.MongoClient("localhost", 27017)
 
@@ -33,28 +43,25 @@ for record in collection.find():
         mrm_string = 'Temperature_Vec_' + str(mrm_index+1)
         #print(mrm_string)
 
-        #a = record[mrm_string]['A']
         values[record_index][mrm_index][0] = record[mrm_string]['A']
         #print(values[record_index][mrm_index][0])
 
-        #b = record[mrm_string]['B']
         values[record_index][mrm_index][1] = record[mrm_string]['B']
         #print(values[record_index][mrm_index][1])
 
-        #c = record[mrm_string]['C']
         values[record_index][mrm_index][2] = record[mrm_string]['C']
         #print(values[record_index][mrm_index][2])
 
-        #mean = record[mrm_string]['Mean']
         values[record_index][mrm_index][3] = record[mrm_string]['Mean']
         #print(values[record_index][mrm_index][3])
 
         #print("*** END OF MRM ***")
 
     #print("*** END OF RECORD ***")
-    record_index += record_index
+    record_index += 1
 
-print(values[0])
+#print(values)
+#print(values[0])
 
 #mylist = list(cursor)
 #print(mylist)
@@ -63,5 +70,27 @@ print(values[0])
 
 
 ####### Plotting using plotly
-fig = FF.create_annotated_heatmap(values[0])
-pl.iplot(fig, filename='mrm_temperature_status')
+mrm_label = ['MRM 1', 'MRM 2', 'MRM 3', 'MRM 4', 'MRM 5', 'MRM 6', 'MRM 7', 'MRM 8', 'MRM 9',]
+temperature_label = ['A', 'B', 'C', 'Mean']
+#bupu = cl.scales['9']['seq']['BuPu']
+#bupu500 = cl.interp( bupu, 500 ) # Map color scale to 500 bins
+#colorscale = [[0, '#3D9970'], [150, '#001f3f']]
+
+# Transpose x and y in array
+values_trans = numpy.array(values[1]).transpose()
+
+# Plot
+fig = FF.create_annotated_heatmap(values_trans, x=mrm_label, y=temperature_label, colorscale='Jet') # 'Viridis'
+fig['layout'].update({
+    'title':'HFF_MRM_Temperature_Status',
+    'autosize':False,
+    'width':'1000',
+    'height':'600',
+    #'margin':go.Margin(l=50, r=50, b=100, t=100, pad=4)
+    #'paper_bgcolor':'#7f7f7f',
+    #'plot_bgcolor':'#c7c7c7'
+})
+# Cloud plotting
+#py.iplot(fig, filename='mrm_temperature_status')
+# Offline plotting
+plotly.offline.plot(fig, filename='mrm_temperature_status.html')
